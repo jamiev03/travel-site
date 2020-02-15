5012a8d21b23353e85bd98548ca6a707726fc7b8
Jamie <jamie03.v@gmail.com>
2020-02-14 20:22:54 -0500

    header design and navigation

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,19 @@ import '../styles/style.css'
 if(module.hot) {
     module.hot.accept()
 };
+
+/*Lesson example code */
+
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function () {
+        console.log('Hi there!!!!');
+    }
+}
+
+let joe = new Person('Joey smith', 'orange');
+joe.greet();
+
+let jane = new Person('Jane thing', 'blue');
+jane.greet();
