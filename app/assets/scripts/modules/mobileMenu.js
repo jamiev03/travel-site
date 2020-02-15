dc0b19e46f02424c808616941d06c67a7671d634
Jamie <jamie03.v@gmail.com>
2020-02-15 16:34:06 -0500

    add new class toggle for nav backgroud ease in and apply applicable styling in css.

--- app/assets/scripts/modules/mobileMenu.js
+++ app/assets/scripts/modules/mobileMenu.js
@@ -2,6 +2,7 @@ class mobileMenu {
     constructor() {
         this.menuIcon = document.querySelector('.site-header__menu-icon')
         this.menuContent = document.querySelector('.site-header__menu-content')
+        this.siteHeader = document.querySelector('.site-header')
         this.events()
         // document.querySelector('.site-header__menu-icon').addEventListener('click', function() {
         //     alert('this is work yes?')
@@ -13,6 +14,7 @@ class mobileMenu {
 
     toggleTheMenu() {
         this.menuContent.classList.toggle('site-header__menu-content--is-visible')
+        this.siteHeader.classList.toggle('site-header--is-expanded')
     }
 }
 
