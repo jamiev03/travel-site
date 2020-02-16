daa830288611ca68d6b6a9eb1128a425559e1e66
Jamie <jamie03.v@gmail.com>
2020-02-16 17:25:09 -0500

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../styles/style.css'
 import mobileMenu from './modules/mobileMenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
+import StickyHeader from './modules/sticky-header';
 
+
+let stickyHeader = new StickyHeader();
 let MobileMenu = new mobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
