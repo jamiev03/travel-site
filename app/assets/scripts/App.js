d1f55bf61de26ec8b0e4caca050ffb4e44388ae7
Jamie <jamie03.v@gmail.com>
2020-02-16 13:07:36 -0500

    improve scroll efficiency by stopping function after items are revealed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/style.css'
 import mobileMenu from './modules/mobileMenu';
+import RevealOnScroll from './modules/reveal-on-scroll';
 
 let MobileMenu = new mobileMenu();
+let revealOnScroll = new RevealOnScroll();
 
 if(module.hot) {
     module.hot.accept()
